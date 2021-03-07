35f8a3e88f29e0a72eff1bd42df76552bea88a0a
Khun Htetz Naing <khunhtetznaing@gmail.com>
2021-03-07 18:12:29 +0630

    Update ovpn_installer.sh

--- ovpn_installer.sh
+++ ovpn_installer.sh
@@ -5,6 +5,7 @@
 # Email: khunhtetznaing@gmail.com
 ############################################################
 
+echo ""
 OPENVPN_FILE="https://github.com/KhunHtetzNaing/Files/releases/download/malVPN/openvpn-as-2.5-CentOS7.x86_64.rpm"
 CRACK_FILE_PATH="/usr/local/openvpn_as/lib/python2.7/site-packages/pyovpn-2.0-py2.7.egg"
 CRACK_FILE_LINK="https://github.com/KhunHtetzNaing/Files/releases/download/malVPN/pyovpn-2.0-py2.7.egg"
@@ -33,7 +34,7 @@ yes
 
 
 " > /dev/null 2>&1
-echo "- DONE ;)"
+echo "  - DONE ;)"
 DEVICE=$(ls -l /sys/class/net | awk '$NF~/pci0/ { print $(NF-2); exit }')
 IPADDR=$(ip -br address show dev $DEVICE | awk '{print substr($3,1,index($3,"/")-1);}')
 echo ""
@@ -42,3 +43,4 @@ echo "Client UI: https://$IPADDR:943/"
 echo "Login as \"openvpn\" with the same password used to authenticate to this UNIX host."
 echo ""
 echo "Type \"passwd openvpn\" for set a password."
+echo ""
